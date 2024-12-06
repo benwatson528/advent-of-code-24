83c132a271f8403187be608821403fb4e883d574
Ben Watson <ben.watson@nandos.co.uk>
2024-12-06 12:26:20 +0000

    Day 6 both parts

---
+++ main/day06/guard_gallivant.py
@@ -0,0 +1,28 @@
+DIRECTIONS = {"^": ">", ">": "v", "v": "<", "<": "^"}
+MOVEMENTS = {"^": (0, -1), ">": (1, 0), "v": (0, 1), "<": (-1, 0)}
+
+
+def solve_p1(obstructions, start) -> int:
+    return len({c for c, d in move(start, "^", obstructions)})
+
+
+def solve_p2(obstructions, start) -> int:
+    original_visited = move(start, "^", obstructions)
+    return sum(len(move(start, "^", obstructions | {c})) == 0 for c in {c for c, _ in original_visited if c != start})
+
+
+def move(current, direction, obstructions):
+    visited = {(current, direction)}
+    max_x, max_y = max(x for x, _ in obstructions), max(y for y, _ in obstructions)
+    while 0 <= current[0] <= max_x and 0 <= current[1] <= max_y:
+        next_step = current[0] + MOVEMENTS[direction][0], current[1] + MOVEMENTS[direction][1]
+        if next_step in obstructions:
+            direction = DIRECTIONS[direction]
+        elif (next_step, direction) in visited:
+            return set()
+        else:
+            current = next_step
+        visited.add((current, direction))
+
+    visited.remove((current, direction))
+    return visited
---
+++ tests/day06/test_guard_gallivant.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+
+from main.day06.guard_gallivant import solve_p1, solve_p2
+
+
+def test_p1_simple():
+    assert solve_p1(*read_input("data/test_input.txt")) == 41
+
+
+def test_p1_real():
+    assert solve_p1(*read_input("data/input.txt")) == 4826
+
+
+def test_p2_simple():
+    assert solve_p2(*read_input("data/test_input.txt")) == 6
+
+
+def test_p2_real():
+    assert solve_p2(*read_input("data/input.txt")) == 1721
+
+
+def read_input(file_name):
+    with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
+        obstructions = set()
+        arr = f.read().splitlines()
+        for y, row in enumerate(arr):
+            for x, col in enumerate(row):
+                if col == "#":
+                    obstructions.add((x, y))
+                elif col == "^":
+                    start = (x, y)
+        return obstructions, start
