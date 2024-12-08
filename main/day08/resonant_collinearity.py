abc1c254deb34940bb5f8184130e95e920debd54
Ben Watson <benwatson528@gmail.com>
2024-12-08 12:55:04 +0000

    Day 8 part 2

--- main/day08/resonant_collinearity.py
+++ main/day08/resonant_collinearity.py
@@ -5,17 +5,18 @@ manhattan = lambda x1, y1, x2, y2: abs(x1 - x2) + abs(y1 - y2)
 in_line = lambda x1, y1, x2, y2, x3, y3: (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2 == 0
 
 
-def solve(antennas, bounds) -> int:
+def solve(antennas, grid_edge, is_expanded=False) -> int:
     return len({antinodes for posns in antennas.values() for pair in combinations(posns, 2) for antinodes in
-                find_antinode_locations(pair, bounds)})
+                find_antinode_locations(pair, grid_edge, is_expanded)})
 
 
-def find_antinode_locations(pair, bounds):
-    return {(x, y) for x in range(bounds[0] + 1) for y in range(bounds[1] + 1) if is_valid_antinode(x, y, pair)}
+def find_antinode_locations(pair, grid_edge, is_expanded):
+    return {(x, y) for x in range(grid_edge + 1) for y in range(grid_edge + 1) if
+            is_valid_antinode(x, y, pair, is_expanded)}
 
 
-def is_valid_antinode(x, y, pair):
+def is_valid_antinode(x, y, pair, is_expanded):
     manhattans = [manhattan(x, y, *p) for p in pair]
-    return ((x, y) not in pair
-            and (manhattans[0] == 2 * manhattans[1] or manhattans[1] == 2 * manhattans[0])
-            and in_line(x, y, *pair[0], *pair[1]))
+    is_valid_distance = abs(manhattans[0] - manhattans[1]) // manhattan(*pair[0], *pair[1]) == 1 if is_expanded else (
+            manhattans[0] == 2 * manhattans[1] or manhattans[1] == 2 * manhattans[0])
+    return is_valid_distance and in_line(x, y, *pair[0], *pair[1])
--- tests/day08/test_resonant_collinearity.py
+++ tests/day08/test_resonant_collinearity.py
@@ -10,7 +10,15 @@ def test_p1_simple():
 
 
 def test_p1_real():
-    assert solve(*read_input("data/input.txt")) == 344  # too low
+    assert solve(*read_input("data/input.txt")) == 344
+
+
+def test_p2_simple():
+    assert solve(*read_input("data/test_input.txt"), is_expanded=True) == 34
+
+
+def test_p2_real():
+    assert solve(*read_input("data/input.txt"), is_expanded=True) == 1182
 
 
 def read_input(file_name):
@@ -20,4 +28,4 @@ def read_input(file_name):
             for x, a in enumerate(row.strip()):
                 if a != ".":
                     antennas[a].append((x, y))
-    return antennas, (x, y)
+    return antennas, y
