a75dc420baf29979580e3c4f5a3595b1f66a00c5
Ben Watson <benwatson528@gmail.com>
2024-12-12 17:31:46 +0000

    Day 12 part 2

--- main/day12/garden_groups.py
+++ main/day12/garden_groups.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def solve(garden, count_straight=False) -> int:
     return sum(find_regions(plants, count_straight) for plants in garden.values())
 
@@ -5,7 +8,7 @@ def solve(garden, count_straight=False) -> int:
 def find_regions(plants, count_straight):
     regions = []
     for plant in plants:
-        if plant not in [x for xs in regions for x in xs]:
+        if plant not in [p for ps in regions for p in ps]:
             regions.append(build_region(plant, plants))
     return sum(price_region(r, count_straight) for r in regions)
 
@@ -25,9 +28,22 @@ def build_region(plant, plants):
 
 def price_region(region, count_straight):
     if count_straight:
-        return 0
+        return len(region) * sum(find_line_matches(region, d, r) for d, r in itertools.product([1, -1], [True, False]))
     else:
         return len(region) * sum(4 - sum([p in get_neighbours(*plant) for p in region]) for plant in region)
+
+
+def find_line_matches(plants, direction, reverse=False):
+    if reverse:
+        plants = [(y, x) for x, y in plants]
+    edges = 0
+    for wall in range(min(y for x, y in plants), max(y for x, y in plants) + 1):
+        plants_in_line = sorted(
+            [p for p in plants if p[1] == wall and (p[0], p[1] + direction) not in plants],
+            key=lambda p: p[0])
+        connected_plants = sum(abs(x1 - x2) == 1 for (x1, _), (x2, _) in zip(plants_in_line, plants_in_line[1:]))
+        edges += len(plants_in_line) - connected_plants
+    return edges
 
 
 get_neighbours = lambda x1, y1: [(x1 - 1, y1), (x1 + 1, y1), (x1, y1 - 1), (x1, y1 + 1)]
--- tests/day12/test_garden_groups.py
+++ tests/day12/test_garden_groups.py
@@ -41,6 +41,10 @@ def test_p2_simple_large():
     assert solve(read_input("data/test_input_large.txt"), count_straight=True) == 1206
 
 
+def test_p2_real():
+    assert solve(read_input("data/input.txt"), count_straight=True) == 808796
+
+
 def read_input(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
         garden = defaultdict(list)
