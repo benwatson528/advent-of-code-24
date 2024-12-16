550930fce8f74b854d29f556475087488b7b88df
Ben Watson <ben.watson@nandos.co.uk>
2024-12-16 18:20:28 +0000

    Day 16 in progress

--- main/day16/reindeer_maze.py
+++ main/day16/reindeer_maze.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, defaultdict
 from typing import Set
 
 DIRECTIONS = {'v': (0, 1), '^': (0, -1), '>': (1, 0), '<': (-1, 0)}
@@ -12,24 +12,34 @@ def solve(walls, start, end, direction='>') -> (int, Set[str]):
 
 def find_min_path(current, direction, walls, end):
     to_visit = deque()
-    to_visit.append((current, direction, 0, []))
+    total_visited = defaultdict(set)
+    to_visit.append((current, direction, 0, {current}))
     visited = {}
     while to_visit:
-        current, direction, score, path_travelled = to_visit.popleft()
+        current, direction, score, tiles_visited = to_visit.popleft()
         if (current, direction) in visited and score > visited[(current, direction)][0]:
             continue
+        # elif (current, direction) in visited and score == visited[(current, direction)][0]:
+        #     total_visited[current].update(visited[(current, direction)][1])
         elif current in walls:
             continue
 
-        visited[(current, direction)] = (score, path_travelled)
-        to_visit.append((move(current, direction), direction, score + 1, path_travelled + [current]))
-        to_visit.append((current, CLOCKWISE_TURNS[direction], score + 1000, path_travelled + [current]))
-        to_visit.append((current, ANTICLOCKWISE_TURNS[direction], score + 1000, path_travelled + [current]))
+        if (current, direction) in visited and score < visited[(current, direction)][0]:
+            total_visited[(current, direction)] = tiles_visited
+        else:
+            total_visited[(current, direction)].update(tiles_visited)
 
+        visited[(current, direction)] = (score, tiles_visited | {current})
+        to_visit.append((move(current, direction), direction, score + 1, tiles_visited | {current}))
+        to_visit.append((current, CLOCKWISE_TURNS[direction], score + 1000, tiles_visited | {current}))
+        to_visit.append((current, ANTICLOCKWISE_TURNS[direction], score + 1000, tiles_visited | {current}))
+
+
+    lowest_end_paths = {k: v for k, v in total_visited.items() if k[0] == end}
 
     min_score = min(s[0] for t, s in visited.items() if t[0] == end)
     shortest = {t: s for t, s in visited.items() if t[0] == end and s[0] == min_score}
-    return min_score
+    return shortest
 
 
 move = lambda o, m: (o[0] + DIRECTIONS[m][0], o[1] + DIRECTIONS[m][1])
--- tests/day16/test_reindeer_maze.py
+++ tests/day16/test_reindeer_maze.py
@@ -5,19 +5,19 @@ from main.day16.reindeer_maze import solve
 
 
 def test_p1_simple():
-    assert solve(*read_input("data/test_input.txt"))[0] == 7036
+    assert solve(*read_input("data/test_input.txt")) == 7036
 
 
 def test_p1_real():
-    assert solve(*read_input("data/input.txt"))[0] == 101492
+    assert solve(*read_input("data/input.txt")) == 101492
 
 
 def test_p2_simple():
-    assert len(solve(*read_input("data/test_input.txt"))[1]) == 45
+    assert solve(*read_input("data/test_input.txt")) == 45
 
 
 def test_p2_real():
-    assert len(solve(*read_input("data/input.txt"))[1]) == -1  # 595 too high
+    assert solve(*read_input("data/input.txt")) == 543
 
 
 def read_input(file_name):
