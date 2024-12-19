86b4dc2eb8a2e55e76435aa6da392e76bde7a2f6
Ben Watson <ben.watson@nandos.co.uk>
2024-12-19 08:50:22 +0000

    Removing pointless counter

--- main/day19/linen_layout.py
+++ main/day19/linen_layout.py
@@ -2,15 +2,15 @@ from functools import cache
 
 
 def solve_p1(patterns, designs) -> int:
-    return sum(x != 0 for x in [is_possible(design, frozenset(patterns), 0) for design in designs])
+    return sum(x != 0 for x in [is_possible(design, frozenset(patterns)) for design in designs])
 
 
 def solve_p2(patterns, designs) -> int:
-    return sum(is_possible(design, frozenset(patterns), 0) for design in designs)
+    return sum(is_possible(design, frozenset(patterns)) for design in designs)
 
 
 @cache
-def is_possible(design, patterns, combinations):
+def is_possible(design, patterns):
     return 1 if len(design) == 0 else sum(
-        is_possible(design.removeprefix(p), patterns, combinations + 1) for p in
+        is_possible(design.removeprefix(p), patterns) for p in
         [p for p in patterns if design.startswith(p)])
--- tests/day19/test_linen_layout.py
+++ tests/day19/test_linen_layout.py
@@ -1,8 +1,6 @@
 import os
 from pathlib import Path
 
-import pytest
-
 from main.day19.linen_layout import solve_p1, solve_p2
 
 
@@ -10,7 +8,6 @@ def test_p1_simple():
     assert solve_p1(*read_input("data/test_input.txt")) == 6
 
 
-@pytest.mark.skip("Takes 6s to run")
 def test_p1_real():
     assert solve_p1(*read_input("data/input.txt")) == 317
 
@@ -19,7 +16,6 @@ def test_p2_simple():
     assert solve_p2(*read_input("data/test_input.txt")) == 16
 
 
-@pytest.mark.skip("Takes 6s to run")
 def test_p2_real():
     assert solve_p2(*read_input("data/input.txt")) == 883443544805484
 
