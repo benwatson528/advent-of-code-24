abc001bfda3a52dafc4aa933ee8202279e396846
Ben Watson <benwatson528@gmail.com>
2024-12-19 08:05:02 +0000

    Day 19 part 2

--- main/day19/linen_layout.py
+++ main/day19/linen_layout.py
@@ -1,16 +1,16 @@
 from functools import cache
 
 
-def solve(patterns, designs) -> int:
-    return sum(is_possible(design, frozenset(patterns)) for design in designs)
+def solve_p1(patterns, designs) -> int:
+    return sum(x != 0 for x in [is_possible(design, frozenset(patterns), 0) for design in designs])
+
+
+def solve_p2(patterns, designs) -> int:
+    return sum(is_possible(design, frozenset(patterns), 0) for design in designs)
 
 
 @cache
-def is_possible(design, patterns):
-    next_moves = [p for p in patterns if design.startswith(p)]
-    if len(design) == 0:
-        return True
-    elif not next_moves:
-        return False
-    else:
-        return any(is_possible(design[len(p):], patterns) for p in next_moves)
+def is_possible(design, patterns, combinations):
+    return 1 if len(design) == 0 else sum(
+        is_possible(design.removeprefix(p), patterns, combinations + 1) for p in
+        [p for p in patterns if design.startswith(p)])
--- tests/day19/test_linen_layout.py
+++ tests/day19/test_linen_layout.py
@@ -1,15 +1,27 @@
 import os
 from pathlib import Path
 
-from main.day19.linen_layout import solve
+import pytest
+
+from main.day19.linen_layout import solve_p1, solve_p2
 
 
 def test_p1_simple():
-    assert solve(*read_input("data/test_input.txt")) == 6
+    assert solve_p1(*read_input("data/test_input.txt")) == 6
 
 
+@pytest.mark.skip("Takes 6s to run")
 def test_p1_real():
-    assert solve(*read_input("data/input.txt")) == 317
+    assert solve_p1(*read_input("data/input.txt")) == 317
+
+
+def test_p2_simple():
+    assert solve_p2(*read_input("data/test_input.txt")) == 16
+
+
+@pytest.mark.skip("Takes 6s to run")
+def test_p2_real():
+    assert solve_p2(*read_input("data/input.txt")) == 883443544805484
 
 
 def read_input(file_name):
