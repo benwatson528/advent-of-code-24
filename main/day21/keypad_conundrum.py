93cda7c06bcef30a2d176293f7cdddb6d204cf7c
Ben Watson <ben.watson@nandos.co.uk>
2024-12-24 07:18:42 +0000

    Day 21 not working

--- main/day21/keypad_conundrum.py
+++ main/day21/keypad_conundrum.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, defaultdict
 from itertools import permutations
 
 DIRECTIONS = {"<": (-1, 0), "v": (0, 1), "^": (0, -1), ">": (1, 0)}
@@ -24,10 +24,17 @@ def move_keypad(command, numpad_paths, arrowpad_paths, num_robots):
     numpad_position = 'A'
     top_level_moves = []
     for next_numpad in command:
-        numpad_moves = numpad_paths[(numpad_position, next_numpad)]
-        ans = move_arrows_rec(numpad_moves, num_robots, arrowpad_paths)
-        top_level_moves.extend(ans)
-        # top_level_moves.append("A")
+        shortest = "dasdnifd"*9999
+        paths = numpad_paths[(numpad_position, next_numpad)]
+        if paths:
+            for path in paths:
+                ans = move_arrows_rec(path, num_robots, arrowpad_paths)
+                if len(ans) < len(shortest):
+                    shortest = ans
+        else:
+            shortest = ""
+        top_level_moves.extend(shortest)
+        top_level_moves.append("A")
         numpad_position = next_numpad
     str_ans = "".join(top_level_moves)
     return len(top_level_moves)
@@ -40,25 +47,53 @@ def move_arrows_rec(movements, num_robots, arrowpad_paths):
         robot_position = "A"
         result = ""
         for m in movements:
-            partial = move_arrows_rec(arrowpad_paths[("A", m)] + "A" + arrowpad_paths[(m, "A")], num_robots - 1, arrowpad_paths)
+            shortest = "dfsfdsfgsdf" * 9999
+            paths = arrowpad_paths[(robot_position, m)]
+            if paths:
+                for path in paths:
+                    partial = move_arrows_rec(path + "A", num_robots - 1, arrowpad_paths)
+                    if len(partial) < len(shortest):
+                        shortest = partial
+            else:
+                shortest = ""
             robot_position = m
-            result += partial
+            result += shortest
+        shortest = "dfsfdsfgsdf" * 9999
+        paths = arrowpad_paths[(robot_position, "A")]
+        if paths:
+            for path in paths:
+                partial = move_arrows_rec(path + "A", num_robots - 1, arrowpad_paths)
+                if len(partial) < len(shortest):
+                    shortest = partial
+        else:
+            shortest = ""
+        result += shortest
         return result
 
 
 def build_pad_paths(pad):
-    pad_paths = {(k, k): "" for k in pad.keys()}
+    max_length = 5
+    pad_paths = defaultdict(list)
     valid_coords = set(pad.values())
     for start, end in permutations(pad.items(), 2):
         to_visit = deque([(start[1], [], "")])
         while to_visit:
             coord, visited, direction = to_visit.popleft()
+            if len(visited) > max_length:
+                continue
             if coord not in valid_coords:
                 continue
             elif coord == end[1]:
-                pad_paths[(start[0], end[0])] = "".join(visited + [direction])
-                break
+                pad_paths[(start[0], end[0])].append("".join(visited + [direction]))
+                continue
             else:
                 for d_sign, d_coord in DIRECTIONS.items():
                     to_visit.append(((coord[0] + d_coord[0], coord[1] + d_coord[1]), visited + [direction], d_sign))
-    return pad_paths
+
+    shortest_pad_paths = defaultdict(list)
+    for k in pad.keys():
+        shortest_pad_paths[(k, k)] = []
+    for k, ls in pad_paths.items():
+        shortest_length = len(min(ls, key=len))
+        shortest_pad_paths[k].extend(l for l in ls if len(l) == shortest_length)
+    return shortest_pad_paths
